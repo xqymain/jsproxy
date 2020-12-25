64402ede3406114718425296da4e5191a0b88349
Xing Qingyu <201330010330@hnu.edu.cn>
2020-12-25 17:22:44 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '111',
+  ver: '120',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -64,10 +64,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  assets_cdn: 'https://gfw.srv.pub/assets/',
+  //assets_cdn: 'https://gfw.srv.pub/assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
