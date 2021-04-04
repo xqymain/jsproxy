f70d136b07748b988d169a9fbc79b4e67c85a6f4
Xing Qingyu <201330010330@hnu.edu.cn>
2021-04-04 16:26:48 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '125',
+  ver: '130',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -12,17 +12,11 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'aws-jp': {
-      label: 'AWS日本节点',
+    'hk': {
+      label: '中国香港节点',
       lines: {
         // 主机:权重
-        'aws.srv.pub:8443': 1,
-      }
-    },
-    'vultr-jp': {
-      label: 'Vultr日本节点',
-      lines: {
-        'proxy.srv.pub:8443': 1,
+        'hk.srv.pub:8443': 1,
       }
     },
     'us': {
@@ -52,13 +46,13 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'aws-jp',
+  node_default: 'hk',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
    */
-  node_acc: 'aws-jp',
+  node_acc: 'hk',
 
   /**
    * 静态资源 CDN 地址
